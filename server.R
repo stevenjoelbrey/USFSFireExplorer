51250d8d47ff8d9aafc0d9fb79ef9fa00e91f90d
Steven <stevenjoelbrey@gmail.com>
2018-03-22 08:29:07 -0600

    fire locations are now plotted on a map.

--- server.R
+++ server.R
@@ -1,8 +1,7 @@
-
-
 # Load the USFS fire data 
-df <- get(load("FPA_FOD_1992_2015.RData"))
-rm(FPA_FOD) # we want to be able to refer to the data as df only 
+df <- get(load("FPA_FOD_1000_acres.RData"))
+
+df$STAT_CAUSE_DESCR <- as.character(df$STAT_CAUSE_DESCR)
 
 # NOTE: MANY OF THESE FIRES DO NOT HAVE A CONT_DATE. Most. 
 len <- as.numeric(df$CONT_DATE - df$DISCOVERY_DATE)
@@ -20,7 +19,6 @@ df$month <- str_sub(as.character(df$DISCOVERY_DATE), 6, 7)
 
 
 # Make sure we have something that is all the same, just in case 
-
 shinyServer(function(input, output) {
 
   
@@ -72,14 +70,48 @@ shinyServer(function(input, output) {
       exponentformat = "SI"
     )
     
-    p <- plot_ly(dfp, x = dfp[[xaxis]], y = dfp[[yaxis]], 
-                 color = dfp[[colors]], size =dfp[[dotSize]],
-                 text = dfp[['FIRE_NAME']]
-                 ) %>%
-      layout(xaxis = x, yaxis = y )
-      
-    p
+    # p <- plot_ly(dfp, x = dfp[[xaxis]], y = dfp[[yaxis]], 
+    #              color = dfp[[colors]], size =dfp[[dotSize]],
+    #              text = dfp[['FIRE_NAME']]
+    #              ) %>%
+    #   layout(xaxis = x, yaxis = y )
     
+    # give state boundaries a white border
+    l <- list(color = toRGB("white"), width = 2)
+    
+    # specify some map projection/options
+    g <- list(
+      scope = 'usa',
+      projection = list(type = 'albers usa'),
+      showland = TRUE,
+      landcolor = toRGB("white"),
+      subunitwidth = 1,
+      countrywidth = 1,
+      subunitcolor = toRGB("black"),
+      countrycolor = toRGB("black")
+    )
+    
+    # Style the legend
+    l <- list(
+      font = list(
+      family = "sans-serif",
+      size = 16,
+      color = "#000"),
+      bgcolor = "#E2E2E2",
+      bordercolor = "#FFFFFF",
+      borderwidth = 2)
+    
+    p <- plot_geo(dfp, locationmode = 'USA-states', sizes = c(1, 250))  %>%
+      add_markers(
+        y = dfp[[yaxis]], x = dfp[[xaxis]], 
+        color = dfp[[colors]], size =dfp[[dotSize]], 
+        text = ~paste("Firename:", dfp[["FIRE_NAME"]], "size", dfp[["FIRE_SIZE"]])
+      ) %>%
+      layout(title = 'Fire Program Analysis Fire Occurrence Data',
+             geo=g,
+             legend = l)
+    
+    p
     
   })
   
@@ -129,8 +161,6 @@ shinyServer(function(input, output) {
       hide_colorbar()
     
     p
-    
-    
     
   })
 
